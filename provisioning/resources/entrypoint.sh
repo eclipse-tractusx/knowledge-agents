fa9412e64b25f89cfb7431057dd12cbe4e3aae11
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2024-02-07 12:48:07 +0100

    fix: use the correct invocation syntax to setup intermediate endpoints correctly.

--- provisioning/resources/entrypoint.sh
+++ provisioning/resources/entrypoint.sh
@@ -122,7 +122,7 @@ for ENDPOINT in $ONTOP_PORT ; do  # NOTE: do not double-quote $services here.
   else 
     echo "Invoking intermediate process";
     java $TOOL -cp ./lib/*:./jdbc/* -Dlogback.configurationFile="/opt/ontop/log/logback.xml" -Dlogging.config="/opt/ontop/log/logback.xml" \
-      it.unibz.inf.ontop.cli.Ontop endpoint ${ONTOLOGY_FILE} ${MAPPING_FILE} \
+      it.unibz.inf.ontop.cli.Ontop endpoint ${ONTOLOGY} ${MAPPING} \
       ${PROPERTIES} ${PORTAL} ${DEV} ${ENDPOINT} ${CORS} ${LAZY}&
   fi
 done
