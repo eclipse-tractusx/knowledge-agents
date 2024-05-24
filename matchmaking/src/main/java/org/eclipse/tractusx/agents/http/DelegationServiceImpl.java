badfa70c929751f35961dbf0cb78acd01f456cdc
Oguzhan Balandi <oguzhan.balandi@t-systems.com>
2024-05-24 12:25:36 +0200

    url validation

--- matchmaking/src/main/java/org/eclipse/tractusx/agents/http/DelegationServiceImpl.java
+++ matchmaking/src/main/java/org/eclipse/tractusx/agents/http/DelegationServiceImpl.java
@@ -41,7 +41,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
